b6ed19f1c95e540f8c8901c21c5cbd47bfbc3439
Mirhuseynlii <mirhuseynli@icloud.com>
2021-06-18 00:19:45 +0400

    first commit

---
+++ ProjectFrontEnd/practice/script.js
@@ -0,0 +1,56 @@
+/* 
+let count_1 = x;
+
+let count_2 = y;
+
+let count_3 = x - y;
+
+if (count_3 > 10) {
+    console.log("Deyer 10-dan boyukdur")
+} else if (count_3 < 10) {
+    console.log("Deyer 10-dan kicikdir");
+} else if (count_3 = 0) {
+    console.log("Deyer 0-a beraberdir");
+} else {
+    console.log("deyer menfidir");
+} 
+ */
+
+
+
+
+
+/* var x = parseInt(prompt("Enter a Value"));
+var y = parseInt(prompt("Enter a Value"));
+
+if (x == 50) {
+    console.log("50");
+} else if (y == 50) {
+    console.log("50");
+} else {
+    console.log("50 deyil");
+} */
+
+/* let arr1 = ['j', 'a', 'v', 'a', 's', 'r', 'i', 'p', 't'];
+
+if (arr1.indexOf("a")) {
+    console.log("arrayde a herifi var");
+} else {
+    console.log("arrayde a herifi yoxdur");
+} */
+
+
+/* let li = document.createElement('li');
+
+let body = document.querySelector('body')
+
+body.appendChild(li);
+
+li.innertext = "men js-de li'yem" */
+
+
+let arr = ['elcin', 'gunel', 'movsum'];
+
+console.log(arr.length);
+
+console.log(arr.indexOf('gunel'));
